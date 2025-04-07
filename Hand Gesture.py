f3063d8d90b45d76a8037712a9c24dfb897f0dbb
Abolfazl-TaheriHaghighi <atahry6@gmail.com>
2025-04-07 13:23:20 +0330

    *

--- Hand Gesture.py
+++ Hand Gesture.py
@@ -2,9 +2,18 @@ import cv2
 import mediapipe as mp # type: ignore
 
 Cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
 
 while True:
     success , img = Cap.read()
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for hand in results.multi_hand_landmarks:
+            mp.solutions.drawing_utils.draw_landmarks(img, hand, mpHands.HAND_CONNECTIONS)
 
     cv2.imshow("Image" , img)
     cv2.waitKey(1)
